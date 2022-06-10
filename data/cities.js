3cfc758290e83b76a65e5e1704c1848d433324cc
Beth <bethmelmtv@gmail.com>
2022-06-09 17:20:20 -0700

    worked on controller, data file and setup.sql file, test failing

---
+++ data/cities.js
@@ -0,0 +1,22 @@
+const cities = [
+  {
+    id: '1',
+    city_name: 'Portland',
+    country: 'USA',
+    expensive: false,
+  },
+  {
+    id: '2',
+    city_name: 'New York',
+    country: 'USA',
+    expensive: true,
+  },
+  {
+    id: '3',
+    city_name: 'Los Angeles',
+    country: 'USA',
+    expensive: true,
+  },
+];
+
+module.exports = { cities };
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists cities;
+
+
+CREATE table cities (
+  id BIGINT GENERATED ALWAYS as IDENTITY,
+  city_name VARCHAR NOT NULL,
+  country VARCHAR NOT NULL,
+  expensive BOOLEAN
+);
+
+
+INSERT INTO cities (city_name, country, expensive) VALUES
+('Atlanta', 'USA', true),
+('New Mexico', 'USA', false);
